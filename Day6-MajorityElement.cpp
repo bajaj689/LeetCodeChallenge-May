d4946adaddec80b951ace6dc835237a3ad04b8d3
Ayush Bajaj <30772186+bajaj689@users.noreply.github.com>
2020-05-07 12:17:43 +0530

    Update Day6-MajorityElement.cpp

--- Day6-MajorityElement.cpp
+++ Day6-MajorityElement.cpp
@@ -20,4 +20,24 @@ public:
         
         
     }
+    
+    //Approach : Vote Algorithm
+    //TC : O(n)
+    //SC : O(1)
+    
+     int majorityElement(vector<int>& nums) {
+         
+     int major=num[0], count = 1;
+         
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+            
+        }
+        return major;
+     }
 };
